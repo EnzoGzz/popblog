d56d2d462eb64f70ba3f92ea0ece47add323b12a
Nyamort <rmurat.murat@gmail.com>
2021-12-15 10:54:51 +0100

    Config

--- bootstrap.php
+++ bootstrap.php
@@ -9,9 +9,9 @@ $paths = array(__DIR__."/app/Model/");
 $isDevMode = false;
 $dbParams = array(
     'driver'   => 'pdo_mysql',
-    'user'     => $DB_USERNAME,
-    'password' => $DB_PASSWORD,
-    'dbname'   => $DB_NAME,
+    'user'     => DB_USERNAME,
+    'password' => DB_PASSWORD,
+    'dbname'   => DB_NAME,
 );
 $config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode,useSimpleAnnotationReader: false);
 $entityManager= EntityManager::create($dbParams, $config);
